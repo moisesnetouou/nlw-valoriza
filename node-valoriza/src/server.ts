b3abf83461619e81193c95134e0b89ec504bf134
Moisés <moisesnetored@gmail.com>
2022-06-02 10:13:58 -0400

    feat: Add first and second route test

--- node-valoriza/src/server.ts
+++ node-valoriza/src/server.ts
@@ -1,6 +1,15 @@
 import "dotenv/config";
-import express from 'express';
+import express, { json } from 'express';
 
 const app = express();
+app.use(express.json());
+
+app.get("/test", (request, response) => {
+  return response.send("Olá NLW");
+})
+
+app.post("/teste-post", (request, response) => {
+  return response.send("Ola Nlw Método Post")
+})
 
 app.listen(process.env.PORT || 5000, () => console.log("Server is Running!"))
