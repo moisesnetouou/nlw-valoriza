8eb1b0ca32fa9d67e306c302b583c3faee432878
Moisés <moisesnetored@gmail.com>
2022-06-02 11:11:36 -0400

    feat: Type ORM added and configured in the project.

--- node-valoriza/src/server.ts
+++ node-valoriza/src/server.ts
@@ -1,15 +1,10 @@
+import "reflect-metadata";
 import "dotenv/config";
 import express, { json } from 'express';
 
+import "./database";
+
 const app = express();
 app.use(express.json());
-
-app.get("/test", (request, response) => {
-  return response.send("Olá NLW");
-})
-
-app.post("/teste-post", (request, response) => {
-  return response.send("Ola Nlw Método Post")
-})
 
 app.listen(process.env.PORT || 5000, () => console.log("Server is Running!"))
