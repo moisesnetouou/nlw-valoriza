1f66f2ed768df8d16270ccaf913a00d691d90944
Moisés <moisesnetored@gmail.com>
2022-06-08 16:45:05 -0400

    feat: Service and Controller of Create Functional User.

--- node-valoriza/src/server.ts
+++ node-valoriza/src/server.ts
@@ -1,10 +1,12 @@
 import "reflect-metadata";
 import "dotenv/config";
 import express, { json } from 'express';
+import { router } from './routes';
 
 import "./database";
 
 const app = express();
 app.use(express.json());
+app.use(router);
 
 app.listen(process.env.PORT || 5000, () => console.log("Server is Running!"))
