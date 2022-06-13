e8c6362d10e3aee809fe0c58b1f65733cd66a183
Moisés <moisesnetored@gmail.com>
2022-06-13 09:04:13 -0400

    feat: Exception Handling

--- node-valoriza/src/server.ts
+++ node-valoriza/src/server.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import "dotenv/config";
-import express, { json } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors";
+
 import { router } from './routes';
 
 import "./database";
@@ -8,5 +10,18 @@ import "./database";
 const app = express();
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: "Internal Server Error"
+  })
+})
 
 app.listen(process.env.PORT || 5000, () => console.log("Server is Running!"))
